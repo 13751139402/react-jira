673ac8d0c577689ced8849569783d315680f0b12
bytedance <daixunwei@bytedance.com>
2021-11-15 11:36:54 +0800

    chore: json-server添加middleware

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,7 +1,26 @@
 import React, { FormEvent } from "react";
 
+const apiUrl = process.env.REACT_APP_API_URL;
 export const LoginScreen = () => {
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {};
+  const login = (param: { username: string; password: string }) => {
+    fetch(`${apiUrl}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(param),
+    }).then(async (response) => {
+      if (response.ok) {
+      }
+    });
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const username = (event.currentTarget.elements[0] as HTMLInputElement).value;
+    const password = (event.currentTarget.elements[1] as HTMLInputElement).value;
+    login({ username, password });
+  };
   return (
     <form onSubmit={handleSubmit}>
       <div>
