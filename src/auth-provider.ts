9ecbcce4e7ba6ac35f2b6917d3575c19c7d9f8bd
bytedance <daixunwei@bytedance.com>
2021-11-15 17:02:49 +0800

    feat: jwt权限认证

---
+++ src/auth-provider.ts
@@ -0,0 +1,45 @@
+// jwt: 一切以token为核心,当login时获取token数据,存入localstorage中,在发送请求时带入headers中。
+// 防止cookie自动带上token,造成CORS攻击
+
+// 在真实环境中，如果使用firebase这种第三方auth服务的话，本文件不需要开发者开发
+
+import { User } from "screens/project-list/search-pannel";
+const localStorage = "__auth_provider_token__";
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const getToken = () => window.localStorage.getItem(localStorage);
+
+export const handleUserResponse = ({ user }: { user: User }) => {
+  window.localStorage.setItem(localStorage, user.token || "");
+  return user;
+};
+
+export const login = (data: { username: string; password: string }) => {
+  return fetch(`${apiUrl}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (response) => {
+    if (response.ok) {
+      return handleUserResponse(await response.json());
+    }
+  });
+};
+
+export const register = (data: { username: string; password: string }) => {
+  return fetch(`${apiUrl}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (response) => {
+    if (response.ok) {
+      return handleUserResponse(await response.json());
+    }
+  });
+};
+
+export const logout = () => window.localStorage.removeItem(localStorage);
