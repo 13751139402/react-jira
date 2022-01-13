272cad9e9227c69b745670d1622220bc45f67d50
bytedance <daixunwei@bytedance.com>
2022-01-13 11:56:16 +0800

    chore: 拖拽实现添加笔记，目前react渲染原理不熟，不太懂

--- src/components/drag-and-drop.tsx
+++ src/components/drag-and-drop.tsx
@@ -1,3 +1,4 @@
+// 封装react-beautiful-dnd
 import React, { ReactNode } from "react";
 import {
   Draggable,
@@ -8,13 +9,19 @@ import {
   DroppableProvidedProps,
 } from "react-beautiful-dnd";
 
+// Droppable的children类型为一个函数，Drop改为传入一个ReactNode
 type DropProps = Omit<DroppableProps, "children"> & { children: ReactNode };
 
+// 放下
 export const Drop = ({ children, ...props }: DropProps) => {
+  // Droppable:可以放下的区域
   return (
+    // children为函数时候，组件能给children传参
     <Droppable {...props}>
       {(provided) => {
+        // 如果children为Element则克隆一份进行props赋值
         if (React.isValidElement(children)) {
+          // Droppable要求子元素得带上provided.innerRef和rovided.droppableProps
           return React.cloneElement(children, {
             ...provided.droppableProps,
             ref: provided.innerRef,
@@ -29,18 +36,24 @@ export const Drop = ({ children, ...props }: DropProps) => {
 
 type DropChildProps = Partial<{ provided: DroppableProvided } & DroppableProvidedProps> &
   React.HTMLAttributes<HTMLDivElement>;
-export const DropChild = React.forwardRef<HTMLDivElement, DropChildProps>(({ children, ...props }, ref) => (
-  <div ref={ref} {...props}>
-    {children}
-    {props.provided?.placeholder}
-  </div>
-));
+// React.forwardRef 会创建一个React组件，这个组件能够将其接受的 ref 属性转发到其组件树下的另一个组件中
+// Refs 提供了一种方式，允许我们访问 DOM 节点或在 render 方法中创建的 React 元素
+export const DropChild = React.forwardRef<HTMLDivElement, DropChildProps>(({ children, ...props }, ref) => {
+  return (
+    <div ref={ref} {...props}>
+      {children}
+      {props.provided?.placeholder}
+    </div>
+  );
+});
 
 type DragProps = Omit<DraggableProps, "children"> & { children: ReactNode };
+// 拖拽
 export const Drag = ({ children, ...props }: DragProps) => {
   return (
     <Draggable {...props}>
       {(provided) => {
+        // Draggable要求provided.draggableProps,provided.dragHandleProps,ref为子组件的props
         if (React.isValidElement(children)) {
           return React.cloneElement(children, {
             ...provided.draggableProps,
